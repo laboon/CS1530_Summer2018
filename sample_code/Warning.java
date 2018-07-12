d0f412d8b67faff4d199765646e69b8c9078c04c
laboon <laboon@gmail.com>
2018-07-12 09:20:04 -0400

    fix var reuse

--- sample_code/Warning.java
+++ sample_code/Warning.java
@@ -4,17 +4,17 @@ import java.util.ArrayList;
 
 public class Warning {
 
-    
+
     public static void main(String[] args) {
-	ArrayList<String> al = new ArrayList<String>();
+	ArrayList al = new ArrayList();
 	for (String arg : args) {
 	    al.add(arg);
 	}
 
 	al.add(new Darble());
-	
+
 	int c = 0;
-	
+
 	for (Object a : al) {
 	    c += Integer.parseInt((String) a);
 	}
